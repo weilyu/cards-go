3f537973982346087d09c172436f2fce7d804147
Wei Lyu <lvwei1001@live.com>
2021-11-25 00:09:02 +0900

    Added test to newDeck

---
+++ deck_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %s", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %s", d[len(d)-1])
+	}
+}
